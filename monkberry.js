87199e619559681471738a90ef47b1b5043bf25e
Anton Medvedev <anton@elfet.ru>
2015-06-24 01:32:39 +0700

    first working prototype

--- monkberry.js
+++ monkberry.js
@@ -1,35 +1,102 @@
 (function (window) {
   function Monkberry() {
     this.pool = new Pool();
+    this.templates = {};
+    this.filters = {};
+    this.wrappers = {};
   }
 
-  Monkberry.prototype.append = function (parent, node, children, template, data) {
-    var i, len, self = this;
+  Monkberry.prototype.foreach = function (parent, node, children, template, data) {
+    var i, j, len, childrenSize = Map.size(children);
 
-    for (i = 0, len = children.length - data.length; i < len; i++) {
-      children[i].remove();
+    len = childrenSize - data.length;
+    for (i in children) if (children.hasOwnProperty(i)) {
+      if (len-- > 0) {
+        children[i].remove();
+      } else {
+        break;
+      }
     }
 
-    for (i = 0, len = children.length; i < len; i++) {
-      children[i].update(data[i]);
+    j = 0;
+    for (i in children) if (children.hasOwnProperty(i)) {
+      children[i].update(data[j++]);
     }
 
-    for (i = children.length, len = data.length; i < len; i++) {
-      var view = this.render(template, data[i]);
+    for (j = childrenSize, len = data.length; j < len; j++) {
+      var view = this.render(template, data[j]);
       view.parent = parent;
-      view.remove = (function (i, view) {
+
+      view.appendTo(node);
+      i = Map.push(children, view);
+
+      var removeNodes = view.remove;
+      view.remove = (function (i, view, removeNodes) {
         return function () {
-          node.removeChild(view.dom);
-          children.splice(i, 1);
-          self.pool.push(template, view);
+          removeNodes();
+          Map.remove(children, i);
         };
-      })(i, view);
-      node.appendChild(view.dom);
+      })(i, view, removeNodes);
     }
   };
 
-  Monkberry.prototype.render = function (name, values) {
+  Monkberry.prototype.render = function (name, values, no_cache) {
+    no_cache = no_cache || false;
 
+    if (this.templates[name]) {
+      var view, self = this;
+
+      if (no_cache) {
+        view = this.templates[name]();
+      } else {
+        view = this.pool.pull(name);
+        if (!view) {
+          view = this.templates[name]();
+        }
+      }
+
+      view.appendTo = function (node) {
+        for (var i of view.nodes) {
+          node.appendChild(i);
+        }
+      };
+
+      view.remove = function () {
+        for (var node of view.nodes) {
+          node.parentNode.removeChild(node);
+        }
+        self.pool.push(name, view);
+      };
+
+      if (values !== undefined) {
+        view.update(values);
+      }
+
+      if (this.wrappers[name]) {
+        view = this.wrappers[name](view);
+      }
+
+      return view;
+    } else {
+      throw new Error('Template with name "' + name + '" does not found.');
+    }
+  };
+
+  Monkberry.prototype.prerender = function (name, times) {
+    while (times--) {
+      this.pool.push(name, this.render(name, undefined, true));
+    }
+  };
+
+  Monkberry.prototype.mount = function (templates) {
+    var _this = this;
+    Object.keys(templates).forEach(function (name) {
+      _this.templates[name] = templates[name];
+    });
+  };
+
+  Monkberry.prototype.willWrap = function (name, wrap) {
+    this.wrappers[name] = wrap;
   };
 
   function Pool() {
@@ -49,6 +116,37 @@
     } else {
       return void 0;
     }
+  };
+
+  function Map() {
+  }
+
+  Map.max = function (map) {
+    var max = 0;
+    for (var i in map) if (map.hasOwnProperty(i)) {
+      if (i > max) {
+        max = i;
+      }
+    }
+    return parseInt(max);
+  };
+
+  Map.push = function (map, element) {
+    var max = Map.max(map) + 1;
+    map[max] = element;
+    return max;
+  };
+
+  Map.remove = function (map, i) {
+    delete map[i];
+  };
+
+  Map.size = function (map) {
+    var size = 0;
+    for (var i in map) if (map.hasOwnProperty(i)) {
+      size++;
+    }
+    return size;
   };
 
   window.monkberry = new Monkberry();
