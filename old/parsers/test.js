9c737e0a6c015a5a4344a9983c85a85c61e2ac67
Anton Medvedev <anton@elfet.ru>
2015-10-19 00:40:55 +0700

    delete old

--- old/parsers/test.js
+++
@@ -1,7 +0,0 @@
-var ExpressionParser = require('./expression');
-
-var ep = new ExpressionParser('value + a / 2 | filter(a.s)');
-var ep2 = new ExpressionParser('f+g+h');
-console.log(ep2.variables);
-console.log(ep.variables);
-console.log(ep.toString());
--- old/utils/size.js
+++
@@ -1,9 +0,0 @@
-function size(obj) {
-  var size = 0, key;
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) size++;
-  }
-  return size;
-}
-
-module.exports = size;
