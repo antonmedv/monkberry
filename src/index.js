6c4e91cefc64cbbedefc7c811bdf5174e6e31e34
Anton Medvedev <anton@medv.io>
2016-06-09 23:42:38 +0700

    Refactor draw ast command.

--- src/index.js
+++ src/index.js
@@ -23,78 +23,14 @@ export class Compiler {
 
     return compile(getTemplateName(getBaseName(filename)), ast, this.options, this.globals);
   }
-}
 
-export class Compiler_v1 {
-  constructor() {
-    this.sources = [];
-    this.transforms = {whitespace, entity};
-    this.globals = [];
-  }
+  drawAstTree(filename, code) {
+    let ast = parser.parse(filename, code);
 
-  addSource(name, code) {
-    this.sources.push([name, code]);
-  }
+    // Transform.
+    Object.keys(this.transforms).forEach((key) => this.transforms[key](ast));
 
-  updateConfig() {
-    config.globals = this.globals;
-  }
-
-  compile() {
-    this.updateConfig();
-
-    let root = new Root();
-    let figures = sourceNode(null, '');
-
-    for (let [name, code] of this.sources) {
-      let ast = parser.parse(code, name);
-
-      // Transforms
-      Object.keys(this.transforms).forEach((key) => this.transforms[key](ast));
-
-      var figure = new Figure(this.getTemplateName(name), root);
-
-      if (asLibrary) {
-        figure.perceivedAsLibrary = true;
-      }
-
-      figures.add(ast.compile(figure));
-    }
-
-    var output = sourceNode(null, '');
-    output
-      .add('module.exports = function (monkberry, document) {\n')
-      .add('var __filters = monkberry.filters;\n')
-      .add(root.compile())
-      .add('return {\n')
-      .add(figures.join(',\n'))
-      .add('};\n')
-      .add('};\n');
-
-    return output;
-  }
-
-  drawAstTree() {
-    this.enhanceParsers();
-
-    if (this.sources.length > 0) {
-      let [name, code, parserType, ] = this.sources[0];
-      if (parserType in this.parsers) {
-
-        var parser = this.parsers[parserType];
-        var ast = parser.parse(code, name);
-
-        // Transforms
-        Object.keys(this.transforms).forEach((key) => this.transforms[key](ast, parser));
-
-        return drawGraph(ast);
-
-      } else {
-        throw new Error(`Unknown parser type: ${parserType}.`)
-      }
-    } else {
-      throw new Error('No sources.');
-    }
+    return drawGraph(ast);
   }
 }
 
