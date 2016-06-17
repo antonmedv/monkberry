f7244f1b6d44a6eef37abb2cdeee0341169d6f2b
Anton Medvedev <anton@medv.io>
2016-06-17 13:22:57 +0700

    Add directives spec.

--- spec/directive.spec.js
+++ spec/directive.spec.js
@@ -6,23 +6,52 @@ describe('Directive', function () {
     root = document.createElement('div');
   });
 
-  it('should be trimed from html', function () {
-    var view = Monkberry.render(DirectiveExample, root);
-    expect(view).toBe('<div></div><div></div><div></div>');
-  });
-
-  it('should be bind, update, unbind', function () {
-    var directives = {
-      one: function () {
+  it('should be trimmed from html', function () {
+    function directiveMock() {
+      return function () {
         this.bind = function (node) {
         };
         this.update = function (node) {
         };
         this.unbind = function (node) {
         };
-      }
+      };
+    }
+
+    var directives = {
+      fade: directiveMock(),
+      show: directiveMock(),
+      content: directiveMock()
     };
 
-    //var view = Monkberry.render(DirectiveOne, root, {directives: directives});
+    var view = Monkberry.render(DirectiveExample, root, {directives: directives});
+
+    expect(view).toBe('<div></div><div></div><div></div>');
+  });
+
+  it('methods bind, update, unbind should be called', function () {
+    var directive = function () {};
+    directive.prototype.bind = function (value) {};
+    directive.prototype.update = function (value) {};
+    directive.prototype.unbind = function (value) {};
+
+    spyOn(directive.prototype, 'bind');
+    spyOn(directive.prototype, 'update');
+    spyOn(directive.prototype, 'unbind');
+
+    var directives = {
+      directive: directive
+    };
+    var options = {
+      directives: directives
+    };
+
+    var view = Monkberry.render(DirectiveOne, root, options);
+    view.update({value: true});
+    view.remove();
+
+    expect(directive.prototype.bind).toHaveBeenCalledWith(view.nodes[0]);
+    expect(directive.prototype.update).toHaveBeenCalledWith(true);
+    expect(directive.prototype.unbind).toHaveBeenCalledWith(view.nodes[0]);
   });
 });
